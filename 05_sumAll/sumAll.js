d1c9b9118e03a4060c67de0990634e5945728f1a
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-08-30 11:51:55 +0300

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let answer = 0;
+    if (typeof a === "number" && typeof b === "number" && a >= 0 && b >= 0){
+        while (a !== b) {
+            if (a > b) {
+                answer += b;
+                b++;
+            }
+            else {
+                answer += a;
+                a++;
+            }
+        }
+        return answer + a;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(1, 4));
