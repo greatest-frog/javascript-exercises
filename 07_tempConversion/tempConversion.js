890fe198fda0361312831fbe18ed03d31d74bdc8
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-08-30 12:12:47 +0300

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
+  return Number(((temperature-32)*5/9).toFixed(1));
+}
 
-};
-
-const ctof = function() {
-
+const ctof = function(temperature) {
+  return Number((32+temperature*9/5).toFixed(1));
 };
 
 // Do not edit below this line
