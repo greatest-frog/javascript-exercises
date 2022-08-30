c3dff18c5a7f6592891c57ccb088db60aa096b65
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-08-30 11:39:08 +0300

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    const array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === arguments[i]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
